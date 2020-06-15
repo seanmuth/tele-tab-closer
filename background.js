85e9d010bbf2de835469d69d29d59909e77bdba7
Garrett Edwards <grrttedwards@users.noreply.github.com>
2020-06-15 18:09:29 -0400

    Fix misaligned tab

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
   if (tab.url.match(/https:\/\/.+?\.zoom\.us\/.+?status=success/)) {
     setTimeout(() => {
-			chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
+      chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
     }, 2000)
   }
 }
