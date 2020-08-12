251c4cea370b1f5b18afb285c4472faf05270283
Garrett Edwards <grrttedwards@users.noreply.github.com>
2020-08-12 19:31:53 -0400

    Increase the wait time from 2 to 5 seconds

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
   if (tab.url.match(/https:\/\/.+?\.zoom\.us\/.+?success.*/)) {
     setTimeout(() => {
       chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
-    }, 2000)
+    }, 5000)
   }
 }
 
