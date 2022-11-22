580bae297d1bae3a6a5a1cd74c8707c44f26b62c
Sean Muth <sean.m.muth@gmail.com>
2022-11-22 10:09:58 -0600

    add gcp tele uri

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
-  if (tab.url.match(/https:\/\/tele\.astronomer\.cloud\/.+?success/)) {
+  if (tab.url.match(/https:\/\/tele\.astronomer\.cloud\/.+?success/) || tab.url.match(/https:\/\/tele\.gcp0001\.us\-east4\.astronomer\.io\/.+?success/)) {
     setTimeout(() => {
       chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
     }, 5000)
