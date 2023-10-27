58d169d6a512e64e796e0ed516e634a6bb25c764
Sean Muth <sean.m.muth@gmail.com>
2023-10-27 11:25:00 -0500

    use half-second timeout

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
   if (tab.url.match(/https:\/\/tele\.astronomer\.cloud\/.+?success/) || tab.url.match(/https:\/\/tele\.gcp0001\.us\-east4\.astronomer\.io\/.+?success/)) {
     setTimeout(() => {
       chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
-    }, 5000)
+    }, 500) // half-second timeout
   }
 }
 
